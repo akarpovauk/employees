4c868d3ca972d0cd4aa7dde6d699ef9cfbf3ee4b
Alena <alena.karpov@gmail.com>
2024-12-02 17:46:30 +0000

    feat: add props to employees-list-item

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -7,6 +7,11 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+	const data = [
+		{name: 'John Smith', salary: 1800, increase: false},
+		{name: 'Vasya Pupkin', salary: 300, increase: true},
+		{name: 'Jayne Dough', salary: 5000, increase: false}
+	];
 	return (
 		<div className='app'>
 			<AppInfo />
@@ -14,7 +19,7 @@ function App() {
 				<SearchPanel />
 				<AppFilter/>
 			</div>
-			<EmployeesList/>
+			<EmployeesList data={data}/>
 			<EmployeesAddForm/>
 		</div>
 	);
